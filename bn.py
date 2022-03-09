e33e92e17c3ed7fb1e123b3a16c4ab90285a9549
Mario Leiva <mario.leiva.al@gmail.com>
2022-03-09 14:52:40 -0300

    Fix bug to read sampling file in parallel

--- bn.py
+++ bn.py
@@ -161,7 +161,7 @@ class BayesNetwork:
         self.generator.toCSV(self.path + self.name + 'samples' + time_as_key + '.csv')
         samplesToReturn = []
         # Load the csv and return samples as list
-        with open(self.path + 'samples' + time_as_key + '.csv', 'r') as read_obj:
+        with open(self.path + self.name + 'samples' + time_as_key + '.csv', 'r') as read_obj:
             csv_dict_reader = DictReader(read_obj)
             for row in csv_dict_reader:
                 asdict = dict(row)
